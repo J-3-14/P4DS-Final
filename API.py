692dd48ad4461e8c6c7462cf7451365e366652c8
J3_14 <128934168+J-3-14@users.noreply.github.com>
2023-05-04 07:17:45 -0400

    Add files via upload

---
+++ API.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Requests allows us to make HTTP requests which we will use to get data from an API
+import requests
+# Pandas is a software library written for the Python programming language for data manipulation and analysis.
+import pandas as pd
+# NumPy is a library for the Python programming language, adding support for large, multi-dimensional arrays and matrices, along with a large collection of high-level mathematical functions to operate on these arrays
+import numpy as np
+# Datetime is a library that allows us to represent dates
+import datetime
+import numpy as np
+# Setting this option will print all collumns of a dataframe
+pd.set_option('display.max_columns', None)
+# Setting this option will print all of the data in a feature
+pd.set_option('display.max_colwidth', None)
+
+
+# In[ ]:
+
+
+# Takes the dataset and uses the rocket column to call the API and append the data to the list
+def getBoosterVersion(data):
+    for x in data['rocket']:
+       if x:
+        response = requests.get("https://api.spacexdata.com/v4/rockets/"+str(x)).json()
+        BoosterVersion.append(response['name'])
+
+
+# In[ ]:
+
+
+# Takes the dataset and uses the launchpad column to call the API and append the data to the list
+def getLaunchSite(data):
+    for x in data['launchpad']:
+       if x:
+         response = requests.get("https://api.spacexdata.com/v4/launchpads/"+str(x)).json()
+         Longitude.append(response['longitude'])
+         Latitude.append(response['latitude'])
+         LaunchSite.append(response['name'])
+
+
+# In[ ]:
+
+
+# Takes the dataset and uses the payloads column to call the API and append the data to the lists
+def getPayloadData(data):
+    for load in data['payloads']:
+       if load:
+        response = requests.get("https://api.spacexdata.com/v4/payloads/"+load).json()
+        PayloadMass.append(response['mass_kg'])
+        Orbit.append(response['orbit'])
+
+
+# In[ ]:
+
+
+# Takes the dataset and uses the cores column to call the API and append the data to the lists
+def getCoreData(data):
+    for core in data['cores']:
+            if core['core'] != None:
+                response = requests.get("https://api.spacexdata.com/v4/cores/"+core['core']).json()
+                Block.append(response['block'])
+                ReusedCount.append(response['reuse_count'])
+                Serial.append(response['serial'])
+            else:
+                Block.append(None)
+                ReusedCount.append(None)
+                Serial.append(None)
+            Outcome.append(str(core['landing_success'])+' '+str(core['landing_type']))
+            Flights.append(core['flight'])
+            GridFins.append(core['gridfins'])
+            Reused.append(core['reused'])
+            Legs.append(core['legs'])
+            LandingPad.append(core['landpad'])
+
+
+# In[ ]:
+
+
+spacex_url="https://api.spacexdata.com/v4/launches/past"
+
+
+# In[ ]:
+
+
+response = requests.get(spacex_url)
+
+
+# In[ ]:
+
+
+print(response.content)
+
+
+# In[ ]:
+
+
+static_json_url='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/API_call_spacex_api.json'
+
+
+# In[ ]:
+
+
+response.status_code
+
+
+# In[ ]:
+
+
+data = pd.read_json(static_json_url)
+
+
+# In[ ]:
+
+
+# Get the head of the dataframe
+data.head(1)
+
+
+# In[ ]:
+
+
+# Lets take a subset of our dataframe keeping only the features we want and the flight number, and date_utc.
+data = data[['rocket', 'payloads', 'launchpad', 'cores', 'flight_number', 'date_utc']]
+
+# We will remove rows with multiple cores because those are falcon rockets with 2 extra rocket boosters and rows that have multiple payloads in a single rocket.
+data = data[data['cores'].map(len)==1]
+data = data[data['payloads'].map(len)==1]
+
+# Since payloads and cores are lists of size 1 we will also extract the single value in the list and replace the feature.
+data['cores'] = data['cores'].map(lambda x : x[0])
+data['payloads'] = data['payloads'].map(lambda x : x[0])
+
+# We also want to convert the date_utc to a datetime datatype and then extracting the date leaving the time
+data['date'] = pd.to_datetime(data['date_utc']).dt.date
+
+# Using the date we will restrict the dates of the launches
+data = data[data['date'] <= datetime.date(2020, 11, 13)]
+
+
+# In[ ]:
+
+
+#Global variables 
+BoosterVersion = []
+PayloadMass = []
+Orbit = []
+LaunchSite = []
+Outcome = []
+Flights = []
+GridFins = []
+Reused = []
+Legs = []
+LandingPad = []
+Block = []
+ReusedCount = []
+Serial = []
+Longitude = []
+Latitude = []
+
+
+# In[ ]:
+
+
+BoosterVersion
+
+
+# In[ ]:
+
+
+# Call getBoosterVersion
+getBoosterVersion(data)
+
+
+# In[ ]:
+
+
+# Call getLaunchSite
+getLaunchSite(data)
+
+
+# In[ ]:
+
+
+# Call getPayloadData
+getPayloadData(data)
+
+
+# In[ ]:
+
+
+# Call getCoreData
+getCoreData(data)
+
+
+# In[ ]:
+
+
+launch_dict = {'FlightNumber': list(data['flight_number']),
+'Date': list(data['date']),
+'BoosterVersion':BoosterVersion,
+'PayloadMass':PayloadMass,
+'Orbit':Orbit,
+'LaunchSite':LaunchSite,
+'Outcome':Outcome,
+'Flights':Flights,
+'GridFins':GridFins,
+'Reused':Reused,
+'Legs':Legs,
+'LandingPad':LandingPad,
+'Block':Block,
+'ReusedCount':ReusedCount,
+'Serial':Serial,
+'Longitude': Longitude,
+'Latitude': Latitude}
+
+
+# In[ ]:
+
+
+# Create a data from launch_dict
+df = pd.DataFrame(launch_dict)
+
+
+# In[ ]:
+
+
+# Show the head of the dataframe
+df.head()
+
+
+# In[ ]:
+
+
+# Hint data['BoosterVersion']!='Falcon 1'
+gh = []
+for i in range(len(df)):
+    if df['BoosterVersion'][i] != 'Falcon 1':
+        gh.append([i])
+
+
+# In[ ]:
+
+
+data_falcon9 = df[:][4:]
+
+
+# In[ ]:
+
+
+data_falcon9.loc[:,'FlightNumber'] = list(range(1, data_falcon9.shape[0]+1))
+data_falcon9.reset_index(inplace=True)
+
+
+# In[ ]:
+
+
+data_falcon9.drop(columns='index', inplace=True)
+data_falcon9.head()
+
+
+# In[ ]:
+
+
+data_falcon9.isnull().sum()
+
+
+# In[ ]:
+
+
+# Calculate the mean value of PayloadMass column
+m = data_falcon9['PayloadMass'].mean()
+# Replace the np.nan values with its mean value
+data_falcon9['PayloadMass'].replace(np.nan, m, inplace=True)
+
+
+# In[ ]:
+
+
+data_falcon9.isnull().sum()
+
